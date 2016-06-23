45bba929ecfbe8f5e0264b82738355b86cbdbcb2
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2016-06-22 19:34:33 -0700

    update entry model

--- model/entry.js
+++ model/entry.js
@@ -3,14 +3,12 @@
 const mongoose = require('mongoose');
 
 const entrySchema = mongoose.Schema({
-  username: {type: String, required: true, unique: true}
-  , title: {type: String, required: true}
-  , author: {type: String}
+  title: {type: String, required: true}
   , keywords: [{type: String}]
   , public: {type: Boolean}
   , publishDate: {type: Date, dafault: Date.now }
   , userId: {type: mongoose.Schema.ObjectId, required: true, ref: 'User'}
-
+  , challengeId: {type: mongoose.Schema.ObjectId, ref:'Challenge'}
 });
 
 module.exports = mongoose.model('Entry', entrySchema);
