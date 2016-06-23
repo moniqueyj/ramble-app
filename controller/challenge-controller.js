45ee706aeaa277b6e9214ca12cb0945c45adfa50
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2016-06-22 20:14:15 -0700

    challenge controller

--- controller/challenge-controller.js
+++ controller/challenge-controller.js
@@ -4,17 +4,67 @@ const debug = require('debug')('ramble:challenge-controller');
 const Challenge = require('../model/challenge');
 const httpErrors = require('http-errors');
 
-//createChallenge
 exports.createChallenge = function(challengeData) {
   debug('createChallenge');
-  return new new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     new Challenge(challengeData).save()
-    .then((challenge) =>
+    .then(challenge => resolve(challenge))
+    .catch(err => reject(httpErrors(400, err.message)));
   });
-}
+};
 
-//fetchChallenge
+exports.fetchChallenge = function(id) {
+  debug('fetchChallenge');
+  return new Promise((resolve, reject) => {
+    Challenge.findOne({_id: id})
+    .then(challenge => resolve(challenge))
+    .catch(err => reject(httpErrors(404, err.message)));
+  });
+};
 
-//updateChallenge
+exports.fetchAllChallenges = function(){
+  debug('fetchAllChallenges');
+  return new Promise((resolve, reject) => {
+    Challenge.find({})
+    .then(challenges => resolve(challenges))
+    .catch(err => reject(httpErrors(404, err.message)));
+  });
+};
 
-//deleteChallenge
+exports.updateChallenge = function(id, data) {
+  debug('updateChallenge');
+  return new Promise((resolve, reject) => {
+    if(!id){
+      var err = httpErrors(400, err.message);
+      return reject(err);
+    }
+    if(!data){
+      err = httpErrors(400, err.message);
+      return reject(err);
+    }
+    Challenge.findOne({_id: id})
+  .then(() => {
+    Challenge.update(data)
+    .then(() => {
+      Challenge.findOne({_id: id})
+      .then(resolve)
+      .catch(reject);
+    })
+    .catch(err => reject(httpErrors(400, err.message)));
+  })
+  .catch(err => reject(httpErrors(404, err.message)));
+  });
+};
+
+exports.removeChallenge = function(id) {
+  debug('removeChallenge');
+  return new Promise((resolve, reject) => {
+    Challenge.findOne({_id: id})
+    .then(() => {
+      Challenge.remove({_id: id})
+      .then(resolve)
+      .catch(err => reject(httpErrors(500, err.message)));
+    })
+    .catch(err => reject(httpErrors(404, err.message)));
+  });
+};
