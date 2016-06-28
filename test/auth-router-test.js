45ac3115a78f61e1edea25b86c4bf71a9e7b3ab2
Cortney Lane <cdlane124@gmail.com>
2016-06-27 21:02:08 -0700

    a little more of the auth-router-test is written

--- test/auth-router-test.js
+++ test/auth-router-test.js
@@ -7,3 +7,15 @@ const debug = require('debug')('ramble:auth-router-test');
 const expect = require('chai').expect;
 const request = require('superagent-use');
 const superPromise = require('superagent-promise-plugin');
+
+const authRouter = require('../route/auth-router');
+
+const server = require('../server');
+const port = process.env.PORT || 3000;
+const baseUrl = `http://localhost:${port}/api`;
+
+request.use(superPromise);
+
+describe('testing auth-router module', function(){
+  before
+})
