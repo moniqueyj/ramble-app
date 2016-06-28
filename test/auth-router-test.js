634e0d8c982957bdda024024b6a5021eef9aa27e
Cortney Lane <cdlane124@gmail.com>
2016-06-27 20:52:38 -0700

    started auth-router-test

--- test/auth-router-test.js
+++ test/auth-router-test.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// actually test the router this time
+process.env.APP_SECRET = 'sonething123';
+process.env.MONGODB_URI = 'mongodb://localhost/rambletest';
+
+const debug = require('debug')('ramble:auth-router-test');
+const expect = require('chai').expect;
+const request = require('superagent-use');
+const superPromise = require('superagent-promise-plugin');
