3ffc3718c3d22d5266d719b9743c3ecb375047d1
Cortney Lane <cdlane124@gmail.com>
2016-06-28 19:12:46 -0700

    POST tests on auth-router done; GET tests in progress

--- test/auth-router-test.js
+++ test/auth-router-test.js
@@ -10,6 +10,8 @@ const request = require('superagent-use');
 const superPromise = require('superagent-promise-plugin');
 
 const authRouter = require('../route/auth-router');
+const authController = require('../controller/auth-controller');
+const userController = require('../controller/user-controller');
 
 const server = require('../server');
 const port = process.env.PORT || 3000;
@@ -27,15 +29,60 @@ describe('testing auth-router module', function(){
   });
 
   describe('POST test module', function() {
-    console.log('HIT');
     it('should return a new user/password', (done) => {
       request.post(`${baseUrl}/signup`)
       .send({username: 'testuser', password: 'testpassword'})
-      .then((res) => {
+      .end((err, res) => {
         expect(res.status).to.equal(200);
         done();
-      })
-      .catch(done);
+      });
+    });
+
+    it('should return status 400 for empty body', (done) => {
+      request.post(`${baseUrl}/signup`)
+      .send({})
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        done();
+      });
+    });
+
+    it('should return status 400 for invalid body', (done) => {
+      request.post(`${baseUrl}/signup`)
+      .send({something: 'something!'})
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        done();
+      });
     });
   }); // end POST test module
+
+  describe('GET test module', function(){
+    before((done) => {
+      authController.signup({username: 'test user', password: 'test password'})
+      .then( () => done()).catch(done);
+    });
+    after((done) => {
+      userController.removeAllUsers();
+      done();
+    });
+
+    it('should return status 200', (done) => {
+      request.get(`${baseUrl}/signin`)
+      .auth('test user', 'test password')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+    });
+
+    it('should return status 401 unauthorized', (done) => {
+      request.get(`${baseUrl}/signin`)
+      .auth('testwrong', 'test password')
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        done();
+      });
+    });
+  }); // end GET test module
 }); // end auth-routre testing module
